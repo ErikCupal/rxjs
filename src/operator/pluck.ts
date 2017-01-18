136de4171c1d058e78c33691e9590017b68f848c
Erik Cupal <lapuc.kire@gmail.com>
2017-01-18 23:05:21 +0100

    Added type inference support

--- src/operator/pluck.ts
+++ src/operator/pluck.ts
@@ -21,6 +21,99 @@ import { map } from './map';
  *
  * @see {@link map}
  *
+ * @param {K1} property The first nested property
+ * @return {Observable} Returns a new Observable of property values from the
+ * source values.
+ * @method pluck
+ * @owner Observable
+ */
+export declare function pluck<T, K1 extends keyof T>(this: Observable<T>, property: K1): Observable<T[K1]>;
+/**
+ * Maps each source value (an object) to its specified nested property.
+ *
+ * <span class="informal">Like {@link map}, but meant only for picking one of
+ * the nested properties of every emitted object.</span>
+ *
+ * <img src="./img/pluck.png" width="100%">
+ *
+ * Given a list of strings describing a path to an object property, retrieves
+ * the value of a specified nested property from all values in the source
+ * Observable. If a property can't be resolved, it will return `undefined` for
+ * that value.
+ *
+ * @example <caption>Map every every click to the tagName of the clicked target element</caption>
+ * var clicks = Rx.Observable.fromEvent(document, 'click');
+ * var tagNames = clicks.pluck('target', 'tagName');
+ * tagNames.subscribe(x => console.log(x));
+ *
+ * @see {@link map}
+ *
+ * @param {K1} property The first nested property
+ * @param {K2} property The second nested property
+ * @return {Observable} Returns a new Observable of property values from the
+ * source values.
+ * @method pluck
+ * @owner Observable
+ */
+export declare function pluck<
+    T,
+    K1 extends keyof T,
+    K2 extends keyof T[K1]
+    >(this: Observable<T>, property: K1, property2: K2): Observable<T[K1][K2]>;
+/**
+ * Maps each source value (an object) to its specified nested property.
+ *
+ * <span class="informal">Like {@link map}, but meant only for picking one of
+ * the nested properties of every emitted object.</span>
+ *
+ * <img src="./img/pluck.png" width="100%">
+ *
+ * Given a list of strings describing a path to an object property, retrieves
+ * the value of a specified nested property from all values in the source
+ * Observable. If a property can't be resolved, it will return `undefined` for
+ * that value.
+ *
+ * @example <caption>Map every every click to the tagName of the clicked target element</caption>
+ * var clicks = Rx.Observable.fromEvent(document, 'click');
+ * var tagNames = clicks.pluck('target', 'tagName');
+ * tagNames.subscribe(x => console.log(x));
+ *
+ * @see {@link map}
+ *
+ * @param {K1} property The first nested property
+ * @param {K2} property The second nested property
+ * @param {K3} property The third nested property
+ * @return {Observable} Returns a new Observable of property values from the
+ * source values.
+ * @method pluck
+ * @owner Observable
+ */
+export declare function pluck<
+    T,
+    K1 extends keyof T,
+    K2 extends keyof T[K1],
+    K3 extends keyof T[K1][K2]
+    >(this: Observable<T>, property: K1, property2: K2, property3: K3): Observable<T[K1][K2][K3]>;
+/**
+ * Maps each source value (an object) to its specified nested property.
+ *
+ * <span class="informal">Like {@link map}, but meant only for picking one of
+ * the nested properties of every emitted object.</span>
+ *
+ * <img src="./img/pluck.png" width="100%">
+ *
+ * Given a list of strings describing a path to an object property, retrieves
+ * the value of a specified nested property from all values in the source
+ * Observable. If a property can't be resolved, it will return `undefined` for
+ * that value.
+ *
+ * @example <caption>Map every every click to the tagName of the clicked target element</caption>
+ * var clicks = Rx.Observable.fromEvent(document, 'click');
+ * var tagNames = clicks.pluck('target', 'tagName');
+ * tagNames.subscribe(x => console.log(x));
+ *
+ * @see {@link map}
+ *
  * @param {...string} properties The nested properties to pluck from each source
  * value (an object).
  * @return {Observable} Returns a new Observable of property values from the
